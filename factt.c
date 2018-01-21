132f853948afb2a6410aa34bf778401d92b80e5e
santhiyasanthra <31505577+santhiyasanthra@users.noreply.github.com>
2018-01-21 11:08:51 +0530

    Create factt.c

---
+++ factt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+	int fact=1, n,i;
+	scanf("%d",&n);
+	printf("%d\n",n);
+	for(i=1;i<=n;i++)
+	{
+		fact=fact*i;
+	
+		
+	}
+	printf("%d",fact);
+	
+	 
+
+	return 0;
+}
